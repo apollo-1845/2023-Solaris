4fdb1db67220f13a9007c8832abb0d951ce41172
spamtactics <108055980+spamtactics@users.noreply.github.com>
2024-01-11 16:47:42 +0000

    Added a way to calculate the time difference

--- speedCalculator.py
+++ speedCalculator.py
@@ -1,1 +1,6 @@
-from exif import Image
+import time
+# we will be using time instead of the tutorial's suggested EXIF data because the time function returns the time elapsed
+#with significantly greater precision than the time recorded in the EXIF data.
+start=time.process_time()
+def checkTimeDiff(start):
+    return time.process_time()-start
